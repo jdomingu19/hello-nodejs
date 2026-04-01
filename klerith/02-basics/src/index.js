9251d074d9a47e3a350eb75fc5249564e66b5f3e
Jesús Domínguez <jesusdominguez2004@gmail.com>
2026-04-01 15:55:34 -0500

    feat(klerith/02-basics): add Class 23 imports & exports example - Updated src/index.js to demonstrate CommonJS require usage - Imported the entire exported object and accessed its properties - Applied object destructuring to import only emailTemplate - Added src/js-foundation/01-email-template.js defining a reusable HTML template - Exported the template using module.exports for reuse across files

--- klerith/02-basics/src/index.js
+++ klerith/02-basics/src/index.js
@@ -3,4 +3,21 @@
 // src/index.js
 
 // --- Class 22: package.json scripts ---
-console.log("Hello, from 'src/index.js'!");
+// console.log("Hello, from 'src/index.js'!");
+
+// --- Class 23: imports & exports ---
+
+// Import the entire exported object from 01-email-template
+const templateContent = require("./js-foundation/01-email-template");
+
+console.log(templateContent); // { emailTemplate: '...' }
+console.log(typeof templateContent); // object
+
+console.log(templateContent.emailTemplate); // '<div>...</div>'
+console.log(typeof templateContent.emailTemplate); // string
+
+// Import only the emailTemplate property using object destructuring
+const { emailTemplate } = require("./js-foundation/01-email-template");
+
+console.log(emailTemplate); // '<div>...</div>'
+console.log(typeof emailTemplate); // string
---
+++ klerith/02-basics/src/js-foundation/01-email-template.js
@@ -0,0 +1,19 @@
+// Hello, Node.js! @jdomingu19
+// Udemy Course by @Klerith (2023)
+// src/js-foundation/01-email-template.js
+
+// --- Class 23: imports & exports ---
+
+// Define a reusable HTML email template as a string
+const emailTemplate = `
+<div>
+  <h1>Hi, {{name}}!</h1>
+  <p>Thank you for your order.</p>
+</div>`;
+
+// console.log(emailTemplate);
+
+// Export the template so it can be imported in other files
+module.exports = {
+  emailTemplate,
+};
