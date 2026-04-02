39e18f59feecbe954b3005dd422e536d0b345a64
Jesús Domínguez <jesusdominguez2004@gmail.com>
2026-04-02 09:09:40 -0500

    feat(klerith/02-basics): add Class 28 arrow functions example - Added src/js-foundation/04-arrow-functions.js using arrow syntax - Documented getUserById function with TSdoc for parameters, remarks, and example - Implemented callback pattern with ternary operator for error/success handling - Exported getUserById for reuse in other files - Updated src/index.js to import and execute getUserById with arrow function syntax

--- klerith/02-basics/src/index.js
+++ klerith/02-basics/src/index.js
@@ -8,37 +8,50 @@
 // --- Class 23: imports & exports ---
 
 // Import the entire exported object from 01-template
-const templateContent = require("./js-foundation/01-template");
+// const templateContent = require("./js-foundation/01-template");
 
-console.log(templateContent); // { emailTemplate: '...' }
-console.log(typeof templateContent); // object
+// console.log(templateContent); // { emailTemplate: '...' }
+// console.log(typeof templateContent); // object
 
-console.log(templateContent.emailTemplate); // '<div>...</div>'
-console.log(typeof templateContent.emailTemplate); // string
+// console.log(templateContent.emailTemplate); // '<div>...</div>'
+// console.log(typeof templateContent.emailTemplate); // string
 
 // Import only the emailTemplate property using object destructuring
-const { emailTemplate } = require("./js-foundation/01-template");
+// const { emailTemplate } = require("./js-foundation/01-template");
 
-console.log(emailTemplate); // '<div>...</div>'
-console.log(typeof emailTemplate); // string
+// console.log(emailTemplate); // '<div>...</div>'
+// console.log(typeof emailTemplate); // string
 
 // --- Class 25: Default Environment Variables ---
 
 // Import and execute the destructuring example file
-require("./js-foundation/02-destructuring");
+// require("./js-foundation/02-destructuring");
 
 // --- Class 27: Callbacks ---
 
 // Import the getUserById function from the callbacks module
-const { getUserById } = require("./js-foundation/03-callbacks");
+// const { getUserById } = require("./js-foundation/03-callbacks");
 
 // Define the id to search for and execute the callback function
+// const id = 1;
+// getUserById(id, function (error, user) {
+//   if (error) {
+//     // Handle the error case by throwing a new Error
+//     throw new Error(error);
+//   }
+//   // Log the user object when found
+//   console.log(user); // { id: 1, name: 'Jesús' }
+// });
+
+// --- Class 28: Arrow Functions ---
+
+// Import the getUserById function from the arrow-functions module
+const { getUserById } = require("./js-foundation/04-arrow-functions");
+
+// Call the function with a specific id and handle the result using a callback
 const id = 1;
-getUserById(id, function (error, user) {
-  if (error) {
-    // Handle the error case by throwing a new Error
-    throw new Error(error);
-  }
+getUserById(id, (error, user) => {
+  if (error) throw new Error(error);
   // Log the user object when found
   console.log(user); // { id: 1, name: 'Jesús' }
 });
